0333f37aa9cecd10609530c8834cfde0973dda83
Deb Totten <deborahrtotten@gmail.com>
2018-08-11 22:03:54 -0400

    battery running out

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kids Meal', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grease Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', true);
